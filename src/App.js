d470b4aeb8c62691b89735f976a9a92ab5685566
chae1125 <y72682685@gmail.com>
2024-08-14 01:23:06 +0900

    ✨ Feat : 로그인 구현

--- src/App.js
+++ src/App.js
@@ -1,37 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-import axios from "axios";
-import {useEffect} from "react";
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './pages/login/login';
 
-
-function App() {
-  const sendReq = async() => {
-    const res = await axios.get('http://localhost:3001');
-    console.log(res.data);
-  };
-
-  useEffect(()=>{
-    sendReq();    
-  });
+const App = () => {
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        {/* 로그인 페이지 경로 */}
+        <Route path="/" element={<Login />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
